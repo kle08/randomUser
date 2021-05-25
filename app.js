bb4a77295a4b1d40da51126207b60eb3558cfbb6
iamasianman <kevinle1991@gmail.com>
2021-05-24 19:47:07 -0700

    implemented random user generator with vue.js

---
+++ app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Phan',
+      email: 'jp@gmail.com',
+      gender: 'male',
+      picture: 'https://random.imagecdn.app/500/150'
+    }
+  },
+  methods: {
+
+
+    async getUser() {
+      const res = await fetch('https://randomuser.me/api')
+
+      const { results } = await res.json()
+
+      console.log(results)
+
+      // console.log(this.firstName)
+      this.firstName = results[0].name.first,
+        this.lastName = results[0].name.last,
+        this.email = results[0].email,
+        this.gender = results[0].gender,
+        this.picture = results[0].picture.large
+    }
+  }
+})
+
+app.mount('#app')
